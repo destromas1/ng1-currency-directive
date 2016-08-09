dfb475a653afac34adf72881208e9639487a4f63
Shahjada <shahjada.talukdar@gmail.com>
2016-08-09 15:04:49 +0600

    Handle other Keys

--- ng1-currency-directive.js
+++ ng1-currency-directive.js
@@ -5,15 +5,25 @@
     return {
       restrict: 'A', // only activate on element attribute
       require: '?ngModel', // get a hold of NgModelController
-      link: function (scope, element, attrs, ngModel) { 
+      link: function (scope, element, attrs, ngModel) {
         
-
-        element.bind('change', function (event) {
+        function formatter() {
           
-        });
+        }
+        
+        ngModel.$parsers.push(function (viewValue) {
+          return viewValue.replace(/,/g, '');
+        })
+
+        element.bind('change', formatter);
 
         element.bind('keydown', function (event) {
-          
+          var key = event.keyCode;
+          // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+          // This lets us support copy and paste too
+          if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40))
+            return;
+          $browser.defer(formatter);
         });
       }
         
